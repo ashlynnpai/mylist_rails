ab3e69dd7094f65e0f207234a058918bc2f6d10b
ashlynnpai <ashlynnpai@gmail.com>
2015-05-09 14:14:19 +0000

    add messages

--- app/models/cast.rb
+++ app/models/cast.rb
@@ -1,3 +1,3 @@
 class Cast < ActiveRecord::Base
-  
+
 end
--- app/models/controllers/casts_controller.rb
+++ app/models/controllers/casts_controller.rb
@@ -5,12 +5,16 @@ class CastsController < ApplicationController
   end
 
   def update
-    @cast = Cast.find(params[:id])
+  @cast = Cast.find(params[:id])
+
     if @cast.update(params.require(:cast).permit(:title, :episode, :watched, :favorite))
+      flash[:success] = "Status changed for Episode #{@cast.episode}, #{@cast.title}"
       redirect_to root_path
+    else
+      flash[:danger] = "There was a problem changing the status for Episode #{@cast.episode}, #{@cast.title}"
     end
   end
-  
+
   private
   
 
