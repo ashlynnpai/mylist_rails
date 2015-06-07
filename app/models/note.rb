37f3090a0bd29f5c0853d2a4e8e7d6a539c53bba
ashlynnpai <ashlynnpai@gmail.com>
2015-06-07 19:27:16 +0000

    note validate content

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,4 +2,6 @@ class Note < ActiveRecord::Base
 
   belongs_to :railscast, foreign_key: 'railscast_id'
   
+  validates :content, presence: true
+  
 end
--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -3,5 +3,6 @@ require 'spec_helper'
 describe Note do
   
   it { should belong_to(:railscast) }
+  it { should validate_presence_of(:content) }
   
 end
