0712483631c550e7eb3f51fe9cc759e99d6dd158
Ashlynn Pai <ashlynnpai@gmail.com>
2015-06-27 01:34:44 +0000

    add completions controller

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,8 @@ Rails.application.routes.draw do
   
   resource :dashboard, only: [:show]
   
+  resource :completion, only: [:show]
+  
   get '/register', to: 'users#new'  
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
